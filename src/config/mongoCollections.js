a8bbb8dd4c9acde092da574dfe2281bf4b3aaf50
Katie Ng <ngkatie16@gmail.com>
2023-11-15 13:05:48 -0500

    Database setup

---
+++ src/config/mongoCollections.js
@@ -0,0 +1,18 @@
+import {dbConnection} from './mongoConnection.js';
+
+const getCollectionFn = (collection) => {
+  let _col = undefined;
+
+  return async () => {
+    if (!_col) {
+      const db = await dbConnection();
+      _col = await db.collection(collection);
+    }
+
+    return _col;
+  };
+};
+
+/* List collections */
+export const users = getCollectionFn('users');
+export const employers = getCollectionFn('employers');
---
+++ src/config/settings.js
@@ -0,0 +1,5 @@
+export const mongoConfig = {
+  serverUrl: 'mongodb://127.0.0.1:27017',
+  database: 'async'
+};
+  
