c67ac8eacb2e80f025302d6d009e8cb919bb1fdd
Eric Park <110264234+ericpark25@users.noreply.github.com>
2023-11-21 19:38:13 -0500

    Set up firebase for project; no authentication yet

---
+++ src/firebase/FirebaseFunctions.js
@@ -0,0 +1,15 @@
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signOut,
+  updateProfile,
+  signInWithEmailAndPassword,
+  updatePassword,
+  signInWithPopup,
+  GoogleAuthProvider,
+  sendPasswordResetEmail,
+  EmailAuthProvider,
+  reauthenticateWithCredential,
+} from 'firebase/auth';
+
+// add firebase functions here
