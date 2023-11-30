bad42fba80e4a2367c97a198b88a415b92bb7ed4
Eric Park <110264234+ericpark25@users.noreply.github.com>
2023-11-29 21:41:58 -0500

    Added database functions for postings

--- backend/data/applicants.js
+++ backend/data/applicants.js
@@ -2,7 +2,49 @@ import { applicants } from "../config/mongoCollections.js";
 import { ObjectId } from "mongodb";
 
 let exportedMethods = {
-  async addApplicant() {},
+  async addApplicant(
+    name,
+    email,
+    dateOfBirth,
+    city,
+    state,
+    industry,
+  ) {
+    //validation
+
+    let newApplicant = {
+      name: name,
+      email: email,
+      dateOfBirth: dateOfBirth,
+      city: city,
+      state: state,
+      industry: industry,
+      applied: []
+    };
+
+    const applicantsCollection = await applicants();
+    const insertInfo = await applicantsCollection.insertOne(newApplicant);
+    if (!insertInfo.acknowledged || !insertInfo.insertedId) {
+      throw 'failed to add applicant'
+    }
+
+    const id = insertInfo.insertedId.toString();
+    const applicant = await this.get(id);
+    applicant._id = applicant._id.toString();
+    return applicant;
+  },
+  async getApplicant(applicantId) {
+    //validate and implement
+  },
+  async getAll() {
+    //validate and implement
+  },
+  //deleteApplicant(applicantId) ?
+  //getAll(); -> get all applicants in applicants collection
+  //getApplicant(applicantId) -> get applicant that matches applicantId
+  //updateApplicant(applicantId, updatedFields) -> updatedFields is an object of fields to update for applicant
+  //getPostings(applicantId) -> for each entry in applied[] field in applicant, call getPosting(postingId) and add result into postings[] -> return postings[]
+
 };
 
 export default exportedMethods;
--- backend/data/postings.js
+++ backend/data/postings.js
@@ -75,6 +75,126 @@ let exportedMethods = {
     });
     return postingList;
   },
+  async deletePosting(postingId) {
+    //validation
+    if (!postingId || typeof postingId !== 'string' || postingId.trim() === '') {
+      throw 'Posting ID must be a non empty string';
+    }
+    if (!ObjectId.isValid(postingId)) {
+      throw 'Invalid ObjectID'
+    }
+    const objectId = new ObjectId(postingId);
+    const postingsCollection = await postings();
+    const posting = await postingsCollection.findOne({ _id: objectId });
+
+    if (!posting) {
+      throw 'No posting found with the supplied ID';
+    }
+
+    //delete
+    const deleteResult = await postingsCollection.deleteOne({ _id: objectId });
+    // console.log(deleteResult);
+    if (deleteResult.deletedCount !== 1) {
+      throw 'Deletion failed';
+    }
+    return posting;
+  },
+
+  //updatePosting
+  async updatePosting(postingId, updatedFields) {
+    // validation
+    if (!postingId || typeof postingId !== 'string' || postingId.trim() === '') {
+      throw 'Posting ID must be a non empty string';
+    }
+    if (!ObjectId.isValid(postingId)) {
+      throw 'Invalid ObjectID'
+    }
+    if (!updatedFields || typeof updatedFields !== 'object') {
+      throw 'You must provide an object of updated fields'
+    }
+
+    const postingsCollection = await postings();
+    const currentPosting = await postingsCollection.findOne({ _id: new ObjectId(postingId) });
+
+    if (!currentPosting) {
+      throw 'No posting found with the supplied ID'
+    }
+
+    // update
+
+    // for (const key in updatedFields) {
+    //   if (currentRecipe[key] === updatedFields[key]) {
+    //     throw 'Values from supplied fields must be different from existing fields';
+    //   }
+    // }
+
+    let updatedPosting = await postingsCollection.updateOne(
+      { _id: new ObjectId(postingId) },
+      { $set: updatedFields }
+    );
+      console.log(updatedPosting);
+    if (updatedPosting.acknowledged === false) {
+      throw 'Failed to update posting';
+    }
+
+    updatedPosting = await postingsCollection.findOne({ _id: new ObjectId(postingId) });
+    return updatedPosting;
+  },
+  //getPosting(postingId)
+  //getAll() -> get all postings in postings collection
+  //getPostingsByEmployer(employerId)
+  async getPostingsByEmployer(employerId) {
+    // validate
+    if (!employerId || typeof employerId !== 'string' || employerId.trim() === '' || !ObjectId.isValid(employerId)) {
+      throw 'Invalid employer ID'
+    }
+    if (!ObjectId.isValid(employerId)) {
+      throw 'Invalid ObjectID'
+    }
+
+    const employersCollection = await employers();
+    const employer = await employersCollection.findOne({ _id: new ObjectId(employerId) });
+
+    if (!employer) {
+      throw new Error('No employer with the supplied ID');
+    }
+
+    // query postings
+
+    const postingsCollection = await postings();
+    const employerPostings = await postingsCollection.find({ employerId: employerId }).toArray();
+
+    return employerPostings;
+  },
+  //getApplicants(postingId) -> check if postingId is found in applied[] field for each applicant -> if it is, add to applicants[] and return applicants[]
+  async getApplicants(postingId) {
+    // validation
+    if (!postingId || typeof postingId !== 'string' || postingId.trim() === '') {
+      throw 'Posting ID must be a non empty string';
+    }
+    if (!ObjectId.isValid(postingId)) {
+      throw 'Invalid ObjectID'
+    }
+    if (!updatedFields || typeof updatedFields !== 'object') {
+      throw 'You must provide an object of updated fields'
+    }
+
+    const postingsCollection = await postings();
+    const currentPosting = await postingsCollection.findOne({ _id: new ObjectId(postingId) });
+
+    if (!currentPosting) {
+      throw 'No posting found with the supplied ID'
+    }
+
+    // query applicants
+    const applicantsCollection = await applicants();
+    const applicants = await applicantsCollection.find({ applied: postingId });
+
+    if (!applicants) {
+      return [];
+    }
+    return applicants;
+  },
 };
 
 export default exportedMethods;
